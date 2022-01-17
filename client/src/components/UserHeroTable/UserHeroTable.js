23b33fb5e74b53c19b8c4817663d6d93e17c5827
Josh Roberts <joshua.roberts628@gmail.com>
2022-01-16 22:16:37 -0500

    [backend, frontend] - added more stats to user hero page and added per game stat slider

--- client/src/components/UserHeroTable/UserHeroTable.js
+++ client/src/components/UserHeroTable/UserHeroTable.js
@@ -1,21 +1,26 @@
 import React, { useState, useEffect, useContext } from 'react';
 import {
     Table,
-    Image
+    Image,
+    Checkbox
 } from 'semantic-ui-react'
 import './UserHeroTable.css'
 import TurbodotaContext from '../TurbodotaContext'
 
 import squirrel from '../../assets/squirrel.png';
+import UserMatchHistory from '../UserMatchHistory/UserMatchHistory';
 
 function UserHeroTable(props) {
+  
     const { heroesList } = useContext(TurbodotaContext);
 
     const [column, setColumn] = useState('')
     const [direction, setDirection] = useState('ascending')
     const [sortableHeroStats, setSortableHeroStats] = useState([])
-
     const heroStats = props.heroStats
+
+    const [checked, setChecked] = React.useState(false);
+    const perGame = false
 
     useEffect(() => {
       let tempArr = []
@@ -26,9 +31,18 @@ function UserHeroTable(props) {
           games: heroStats[heroKey].games,
           wins: heroStats[heroKey].wins,
           losses: heroStats[heroKey].losses,
-          winpercentage: ((heroStats[heroKey].wins / heroStats[heroKey].games) * 100)
+          winpercentage: ((heroStats[heroKey].wins / heroStats[heroKey].games) * 100),
+          kills: heroStats[heroKey].kills,
+          deaths: heroStats[heroKey].deaths,
+          assists: heroStats[heroKey].assists,
+          kda: (heroStats[heroKey].kills + heroStats[heroKey].assists) / heroStats[heroKey].deaths,
+          killsPerGame: heroStats[heroKey].kills / heroStats[heroKey].games,
+          deathsPerGame: heroStats[heroKey].deaths / heroStats[heroKey].games,
+          assistsPerGame: heroStats[heroKey].assists / heroStats[heroKey].games
         }
+
         tempArr.push(pushObj)
+
       })
       setSortableHeroStats(tempArr)
       // handleSort('games')
@@ -45,6 +59,7 @@ function UserHeroTable(props) {
           if(a[clickedColumn] > b[clickedColumn]) return 1
           return 0
         })
+        
         console.log('tempSorted: ', tempSorted)
         setSortableHeroStats(tempSorted)
         setColumn(clickedColumn)
@@ -74,9 +89,14 @@ function UserHeroTable(props) {
       }
     }
 
-    return (
+    const handleChange = () => {
+      setChecked(!checked);
+    };
+    
+    return (     
       <Table compact sortable celled fixed>
         <Table.Header>
+        <Checkbox toggle className = "toggle-switch" value={checked} label = "Toggle Per Game" onChange={ handleChange }></Checkbox>
           <Table.Row>
             <Table.HeaderCell
               sorted={column === 'id' ? direction : null}
@@ -87,6 +107,7 @@ function UserHeroTable(props) {
             <Table.HeaderCell
               sorted={column === 'heroname' ? direction : null}
               onClick={handleSort('heroname')}
+              
             >
               Hero
             </Table.HeaderCell>
@@ -114,6 +135,61 @@ function UserHeroTable(props) {
             >
               Losses
             </Table.HeaderCell>
+
+            { !checked ? (
+              <Table.HeaderCell
+              sorted={column === 'kills' ? direction : null}
+              onClick={handleSort('kills')}
+            >
+              Kills
+            </Table.HeaderCell>
+            ) : (
+              <Table.HeaderCell
+              sorted={column === 'kills' ? direction : null}
+              onClick={handleSort('kills')}
+            >
+              Kills/Game
+            </Table.HeaderCell>
+            )}
+            
+            { !checked ? (
+              <Table.HeaderCell
+              sorted={column === 'deaths' ? direction : null}
+              onClick={handleSort('deaths')}
+            >
+              Deaths
+            </Table.HeaderCell>
+            ) : (
+              <Table.HeaderCell
+              sorted={column === 'deaths' ? direction : null}
+              onClick={handleSort('deaths')}
+            >
+              Deaths/Game
+            </Table.HeaderCell>
+            )}
+
+            { !checked ? (
+              <Table.HeaderCell
+              sorted={column === 'assists' ? direction : null}
+              onClick={handleSort('assists')}
+            >
+              Assists
+            </Table.HeaderCell>
+            ) : (
+              <Table.HeaderCell
+              sorted={column === 'assists' ? direction : null}
+              onClick={handleSort('assists')}
+            >
+              Assists/Game
+            </Table.HeaderCell>
+            )}
+
+            <Table.HeaderCell
+              sorted={column === 'kda' ? direction : null}
+              onClick={handleSort('kda')}
+            >
+              KDA
+            </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -126,7 +202,7 @@ function UserHeroTable(props) {
             <Table.Cell>{hero.hero_id}</Table.Cell>
             <Table.Cell>
               <div className='customFlexRow'>
-                <div style={{ marginRight: '1em'}}>
+                <div style={{ marginRight: '1em', overflow: 'auto', whiteSpace: 'nowrap'}}>
                   { heroIcon(hero.hero_id) }
                 </div>
                 <div>
@@ -138,6 +214,22 @@ function UserHeroTable(props) {
             <Table.Cell>{ hero.winpercentage.toFixed(1) }</Table.Cell>
             <Table.Cell>{hero.wins}</Table.Cell>
             <Table.Cell>{hero.losses}</Table.Cell>
+            { !checked ? (
+              <Table.Cell>{hero.kills}</Table.Cell>
+            ) : (
+              <Table.Cell>{hero.killsPerGame.toFixed(1)}</Table.Cell>
+            )}
+            { !checked ? (
+              <Table.Cell>{hero.deaths}</Table.Cell>
+            ) : (
+              <Table.Cell>{hero.deathsPerGame.toFixed(1)}</Table.Cell>
+            )}
+            { !checked ? (
+              <Table.Cell>{hero.assists}</Table.Cell>
+            ) : (
+              <Table.Cell>{hero.assistsPerGame.toFixed(1)}</Table.Cell>
+            )}   
+            <Table.Cell>{ hero.kda.toFixed(1) }</Table.Cell>
           </Table.Row>
         ))}
         </Table.Body>
